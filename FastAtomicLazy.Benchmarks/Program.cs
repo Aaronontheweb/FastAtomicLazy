56b988e61bb3d165a68403b6f47b64de25fb23c2
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-25 18:02:22 -0700

    Updated the project to .NET Standard to allow benchmark .NET vs Core 2.0 vs Core 2.1 (#4)
    * port to .NET Standard
    
    * update test project
    
    * update benchmarks project
    
    * make it compile
    
    * define a config for .NET, .NET Core 2.0 and .NET Core 2.1
    
    * the project can target .NET Standard 1.0

--- FastAtomicLazy.Benchmarks/Program.cs
+++ FastAtomicLazy.Benchmarks/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Aaron Stannard 2016. All rights reserved.
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
 
 namespace FastAtomicLazy.Benchmarks
 {
@@ -9,7 +12,11 @@ namespace FastAtomicLazy.Benchmarks
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<FastLazyBenchmarks>();
+            var summary = BenchmarkRunner.Run<FastLazyBenchmarks>(
+                DefaultConfig.Instance
+                    .With(Job.Clr.WithId("Desktop"))
+                    .With(Job.Core.With(CsProjCoreToolchain.NetCoreApp20).WithId("Core 2.0"))
+                    .With(Job.Core.With(CsProjCoreToolchain.NetCoreApp21).WithId("Core 2.1")));
         }
     }
 }
